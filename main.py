6830e9801623fc81616e76bf35968a644ce67249
Egor <egor2002-1002@mail.ru>
2021-02-17 12:58:45 +1000

    Вывод ключа + доп. правки

--- main.py
+++ main.py
@@ -1,59 +1,79 @@
-# Поменять в строке n и n+1 символы
+alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+frequency_letters = 'оеаинтсрвлкмдпуяыьгзбчйхжшюцщэфъ'
+
+
+# Поменять в строке n и n+1 символы (ф-ия для сортировки)
 def switch_sym_with_next(string, n):
     string = ''.join([string[:n], string[n + 1], string[n], string[n + 2:len(string)]])
     return string
 
 
-alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-frequency_ordered_letters = 'оеаинтсрвлкмдпуяыьгзбчйхжшюцщэфёъ'
+# Получение ключа из последовательности, упорядоченной по частоте встречаемости букв в шифротексте
+def get_key(frequency_letters_in_text_):
+    frequency_letters_ = 'оеаинтсрвлкмдпуяыьгзбчйхжшюцщэфъ'
+    for i in range(0, len(alphabet)):
+        for j in range(0, len(alphabet) - 1):
+            if frequency_letters_[j] > frequency_letters_[j + 1]:
+                frequency_letters_ = switch_sym_with_next(frequency_letters_, j)
+                frequency_letters_in_text_ = switch_sym_with_next(frequency_letters_in_text_, j)
+    return frequency_letters_in_text_
+
+
+# Дешифровка текста, зашифрованного с помощью шифра замены
+def decrypt_substitution_cipher(ciphertext_, key_):
+    plaintext_ = ''
+    for i in range(0, len(ciphertext_)):
+        if alphabet.find(ciphertext_[i]) != -1:
+            plaintext_ += alphabet[key_.find(ciphertext_[i])]
+        elif alphabet.upper().find(ciphertext_[i]) != -1:
+            plaintext_ += alphabet[key_.upper().find(ciphertext_[i])].upper()
+        else:
+            plaintext_ += ciphertext_[i]
+    return plaintext_
+
+
+# Считывание текста из файла
 try:
-    file = open('ciphertext.txt', 'r', encoding="utf-8")
+    file_ciphertext = open('ciphertext.txt', 'r', encoding="utf-8")
 except IOError as e:
     print('Не удалось открыть файл!')
     exit()
-frequency_ordered_letters_in_text = alphabet
-amount = [0] * 33
-# Подсчёт встречаемости букв в заданном тексте
+ciphertext = ''
 while True:
-    ch = file.read(1)
+    ch = file_ciphertext.read(1)
     if ch == '':
         break
-    for i in range(0, len(alphabet)):
-        if ch == alphabet[i] or ch == alphabet[i].upper():
-            amount[i] += 1
+    ciphertext += ch
+file_ciphertext.close()
+# Подсчёт встречаемости букв в заданном тексте
+frequency_letters_in_text = alphabet
+amount = [0] * 33
+for i in range(0, len(ciphertext)):
+    for j in range(0, len(alphabet)):
+        if ciphertext[i] == alphabet[j] or ciphertext[i] == alphabet[j].upper():
+            amount[j] += 1
 # Сортировка
 for i in range(0, len(alphabet)):
     for j in range(0, len(alphabet) - 1):
         if amount[j] < amount[j + 1]:
             amount[j], amount[j + 1] = amount[j + 1], amount[j]
-            frequency_ordered_letters_in_text = switch_sym_with_next(frequency_ordered_letters_in_text, j)
-# Отладка
-print(frequency_ordered_letters_in_text)
-print(amount)
-# Замена букв в тексте в соответствии с частотным анализом
-file.seek(0)
-new_string = ''
-while True:
-    ch = file.read(1)
-    if ch == '':
-        break
-    if alphabet.find(ch) != -1:
-        new_string += frequency_ordered_letters[frequency_ordered_letters_in_text.find(ch)]
-    elif alphabet.upper().find(ch) != -1:
-        new_string += frequency_ordered_letters[frequency_ordered_letters_in_text.upper().find(ch)].upper()
-    else:
-        new_string += ch
-print(new_string)
+            frequency_letters_in_text = switch_sym_with_next(frequency_letters_in_text, j)
+# Получение гипотезы о возможном ключе и исползование его для дешифровки
+key = get_key(frequency_letters_in_text)
+plaintext = decrypt_substitution_cipher(ciphertext, key)
+print(plaintext)
+print("Ключ:")
+print(alphabet)
+print(key)
 # Дополнительные замены
 while True:
     print('\nДополнительные замены ("exit" для выхода)')
     print("Заменить: ")
     first_char = input().lower()
     if first_char == 'exit':
-        file_output = open('planetext.txt', 'w', encoding='utf-8')
-        file_output.write(new_string)
-        file.close()
-        file_output.close()
+        file_plaintext = open('plaintext.txt', 'w', encoding='utf-8')
+        file_plaintext.write(plaintext)
+        file_plaintext.close()
         exit()
     print("И: ")
     second_char = input().lower()
@@ -62,8 +82,11 @@ while True:
         print("Данные введены неверно\n")
         input()
     else:
-        new_string = new_string.replace(first_char, '***').replace(second_char, first_char).replace('***', second_char)
-        new_string = new_string.replace(first_char.upper(), '***').replace(second_char.upper(),
-                                                                           first_char.upper()).replace('***',
-                                                                                                       second_char.upper())
-    print(new_string)
+        first_char = key[alphabet.find(first_char)]
+        second_char = key[alphabet.find(second_char)]
+        key = key.replace(first_char, '*').replace(second_char, first_char).replace('*', second_char)
+        plaintext = decrypt_substitution_cipher(ciphertext, key)
+    print(plaintext)
+    print("Ключ:")
+    print(alphabet)
+    print(key)
